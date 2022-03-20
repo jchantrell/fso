a761cb04fdb038dd949921876130f5e87a4b0e7d
Joel Chantrell <joelchantrell@gmail.com>
2022-03-20 15:17:00 +1030

    exercise 2.4

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,59 +1,71 @@
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <div>
-      <Course course={course} />
+      <Course courses={courses} />
     </div>
   )
 }
 
-const Course = (props) => {
+const Course = (props) => <>{props.courses.map(course => 
+<Content course={course} key={course.id}/>)}</>
+
+const Content = (props) => {
   return (
     <div>
-      <Header text={props.course.name} />
-      <Content parts={props.course.parts} />
+      <Header text={props.course.name} /> 
+      {props.course.parts.map(part => 
+      <Part part={part} key={part.id}/>)}
+      <Total parts={props.course.parts} />
     </div>
   )
 }
 
 const Header = props => <h1>{props.text}</h1>
-
-const Content = (props) => {
-  return (
-    <div>
-      <Part part={props.parts[0]}/>
-      <Part part={props.parts[1]}/>
-      <Part part={props.parts[2]}/>
-      <Total parts={props.parts}/>
-    </div>
-  )
-}
 
 const Part = (props) => {
   return (
@@ -65,7 +77,7 @@ const Total = (props) => {
   const total = props.parts.reduce((a, b) => ({exercises: a.exercises + b.exercises}));
 
   return (
-    <p> total of {total.exercises} exercises</p>
+    <p style={{fontWeight: "bold"}}> total of {total.exercises} exercises</p>
   )  
 }     
 
