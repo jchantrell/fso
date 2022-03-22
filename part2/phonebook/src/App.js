14e0cf7c06235a161b0cfec2824ed771be5abbd0
Joel Chantrell <joelchantrell@gmail.com>
2022-03-22 21:35:29 +1030

    in progress

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,11 +1,16 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([]) 
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-  
-  
+  const [filter, setFilter] = useState('')
+  const [foundPersons, setFoundPersons] = useState(persons)
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -24,21 +29,42 @@ const App = () => {
     setNewNumber('')
   }
 
-  const handlePersonChange = (event) => {
+  const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
+
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
+  }
+
+  const handleFilter = (event) => {
+    const keyword = event.target.value;
+
+    if (keyword !== '') {
+      const results = persons.filter((person) => {
+        return person.name.toLowerCase().startsWith(keyword.toLowerCase());
+      });
+      setFoundPersons(results);
+    } else {
+      setFoundPersons(persons);
+    }
+    setFilter(keyword);
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>      
+        filter <input 
+        value={filter} 
+        onChange={handleFilter} />
+      </div>
+      <h2>Add new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input 
           value={newName}
-          onChange={handlePersonChange}/>
+          onChange={handleNameChange}/>
         </div>
         <div>
           number: <input 
@@ -50,11 +76,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <ul>
-        {persons.map(person => 
-          <Person key={person.name} person={person} />
-        )}
-      </ul>
+      <Persons persons={foundPersons} />
     </div>
   )
 }
@@ -65,9 +87,19 @@ const PersonExists = (persons, newName) => {
   })
 }
 
+const Persons = (props) => {
+  return (
+    <ul>
+      {props.persons.map(person => 
+        <Person key={person.name} person={person} />
+      )}
+    </ul>
+  )
+}
+
 const Person = ({ person }) => {
   return (
-    <li>{person.name} {person.number}</li>
+    <li>{person.name} - {person.number}</li>
   )
 }
 
